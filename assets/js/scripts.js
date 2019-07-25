3e93e3428e0f5f8c8d4a53e278b2e3fd2fcc505e
dajamante <aimaiga2@gmail.com>
2019-07-25 20:07:26 +0200

    fixed swipebox with older version of jquery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-    $(document).ready(function(){
-			$(".navigation_menu-toggle a").click(function(){
-				$(".navigation_menu").slideToggle(700);
+    $(document).ready(function(e){
+			$(".navigation_menu-toggle a").click(function(e){
+				e.stopPropagation();
+				$(".navigation_menu").slideToggle(700)
 			});
 		});
-});
-
-
-$(document).ready(function () {
-	/* Basic Gallery */
-	$('.swipebox').swipebox();
 });
