a73e493bff40a5b845cf60b5319b0611c05a0185
dajamante <aimaiga2@gmail.com>
2019-07-25 10:01:48 +0200

    Updated table with correct ws and wf

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ const LONGITUDE  = 18.1489;
 const API_ADRESS = `https://opendata-download-metfcst.smhi.se/`;
 const LONGLAT = `api/category/pmp3g/version/2/geotype/point/lon/${LONGITUDE}/lat/${LATITUDE}/data.json`
 
-const TABLE_TODAY = document.querySelector("#table_today");
+const TABLE_TEMPERATURES = document.querySelector("#table_of_temperatures");
 
 addEventListener('load', main);
 
@@ -19,7 +19,18 @@ function getResponse(){
   fetch(API_ADRESS+LONGLAT)
   .then(response => response.json())
   .then(data=> {
-    isolateRelevantTimes(data)
+    return getRelevantTimes(data)
+  })
+  .then(array => {
+    /* the array is the relevant temperatures we need, with times 6, 12 and 18*/
+    return createTable(array)
+  })
+  .then(array =>{
+    return appendTemperatures(array)
+  })
+  .then(array => {
+    /* this function does wind force and direction*/
+    return appendOrientedArrow(array)
   })
   .catch(error => console.dir(error))
 }
@@ -27,11 +38,11 @@ function getResponse(){
 /*-------------
 Helpfunction to get the data 6:00, 12:00, 18:00
 -------------*/
-function isolateRelevantTimes(data){
-
+function getRelevantTimes(data){
       let now = new Date().getHours();
       let relevant_times = []
 
+      /* We only want information at 06:00, 12:00, 18:00*/
       for (let i = 0; i < data.timeSeries.length; i++) {
             let current_time = new Date(data.timeSeries[i].validTime);
             if ([6, 12, 18].includes(current_time.getHours())){
@@ -40,20 +51,18 @@ function isolateRelevantTimes(data){
       }
 
       if (now > new Date(relevant_times[0].validTime).getHours()){
-        /* if it's 14:00 we don't need 12:00 and delete the first time of the array
-        */
+        /* if it's 14:00 now, we don't need information given at 12:00.
+        Therefore we delete the first time of the array */
         relevant_times.shift();
       }
-      createTable(relevant_times);
-      console.dir(relevant_times)
-
+      return relevant_times;
 }
 
 /*-------------
-Helpfunction to create the table.
-It calls a function to append temperatures and fixed information,
-and another function to append a rotated arrow.
+Helpfunction to create the empty table. It's followed by functions to append Correct,
+append wind direction and append information on the sky state.
 -------------*/
+
 function createTable(relevant_times){
       for (let i = 0; i < 3; i++) {
           let row = document.createElement("tr")
@@ -61,37 +70,28 @@ function createTable(relevant_times){
               let column = document.createElement("td");
               row.appendChild(column);
           }
-          TABLE_TODAY.appendChild(row);
+          TABLE_TEMPERATURES.appendChild(row);
       }
-      appendTemperature(relevant_times);
+      return relevant_times;
 }
 
-function appendTemperature(relevant_times){
+function appendTemperatures(relevant_times){
+    console.dir(relevant_times)
     var options = { month: 'short', day: 'numeric', weekday: 'short'};
     for (let i = 0; i < 3; i++) {
         let formated_date = new Date(relevant_times[i].validTime).toLocaleDateString("sv-SV", options);
         let formated_hour = new Date(relevant_times[i].validTime).getHours();
-        let temp_index = getTemperatureIndex(relevant_times[i].parameters);
-        let temperature = relevant_times[i].parameters[temp_index].values;
 
-        TABLE_TODAY.rows[i+1].cells[0].innerHTML = formated_date;
-        TABLE_TODAY.rows[i+1].cells[1].innerHTML = formated_hour;
-        TABLE_TODAY.rows[i+1].cells[2].innerHTML = temperature;
+        /*The index appear to vary sometimes in the table, t can be att index 11 or 1*/
+        let temp_index = getCorrectIndex(relevant_times[i].parameters, "t");
+
+        let Correct = relevant_times[i].parameters[temp_index].values;
+
+        TABLE_TEMPERATURES.rows[i+1].cells[0].innerHTML = formated_date;
+        TABLE_TEMPERATURES.rows[i+1].cells[1].innerHTML = formated_hour+":00";
+        TABLE_TEMPERATURES.rows[i+1].cells[2].innerHTML = Correct;
         }
-        appendOrientedArrow(relevant_times);
-}
-/*------------------------------
-Help function to get correct index for temperature, as it variates.
---------------------------------*/
-function getTemperatureIndex(parameters_array){
-  let correct_index;
-  for (let j = 0; j < parameters_array.length; j++) {
-            if(parameters_array[j].name == "t"){
-            correct_index = j;
-            break;
-            }
-      }
-      return correct_index;
+        return relevant_times;
 }
 
 function appendOrientedArrow(relevant_times){
@@ -99,30 +99,32 @@ function appendOrientedArrow(relevant_times){
         let arrow = new Image(20, 20);
         let windSpeed = document.createElement('div')
         arrow.src = '../assets/images/small/arrow.jpg';
-        let wd_index = getWindDirectionIndex(relevant_times[i].parameters);
+        let wd_index = getCorrectIndex(relevant_times[i].parameters, "wd");
         let arrowDegrees = (relevant_times[i].parameters[wd_index].values)
-        arrow.style.transform = `rotate(${arrowDegrees}deg)`;
+        /* +90 since my arrow is oriented east while smhi arrow is oriented south
+        yeah I know it's a quick image fix but...*/
+        arrow.style.transform = `rotate(${arrowDegrees+90}deg)`;
 
-        windSpeed.innerHTML = "("+relevant_times[i].parameters[14].values+")"
+        let windSpeedIndex = getCorrectIndex(relevant_times[i].parameters, "ws")
 
-        TABLE_TODAY.rows[i+1].cells[3].appendChild(arrow)
-        TABLE_TODAY.rows[i+1].cells[3].appendChild(windSpeed)
+        windSpeed.innerHTML = "("+relevant_times[i].parameters[windSpeedIndex].values+")"
+
+        TABLE_TEMPERATURES.rows[i+1].cells[3].appendChild(arrow)
+        TABLE_TEMPERATURES.rows[i+1].cells[3].appendChild(windSpeed)
       }
+      return relevant_times
 }
 /*------------------------------
-Help function to get correct index for wind direction, as it variates.
+Help function to get correct index for temperature, wind direction and wind force,
+as the placement variates in the array.
 --------------------------------*/
-function getWindDirectionIndex(parameters_array){
+function getCorrectIndex(parameters_array, searchedItem){
   let correct_index;
   for (let j = 0; j < parameters_array.length; j++) {
-            if(parameters_array[j].name == "wd"){
+            if(parameters_array[j].name == searchedItem){
             correct_index = j;
             break;
             }
       }
       return correct_index;
 }
-
-/*------------------------------
-Help function to get correct index for wind speed, as it variates!!!!
---------------------------------*/
