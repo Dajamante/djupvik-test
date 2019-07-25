815f81f3c19ef2ce574b76debb040ce4836b88a3
dajamante <aimaiga2@gmail.com>
2019-07-25 17:27:51 +0200

    included menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,7 @@ function main(){
     removeEventListener('load', main);
     getResponse();
 }
+
 /*-------------
 Fetcher
 -------------*/
---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(document).ready(function(){
+			$(".navigation_menu-toggle a").click(function(){
+				$(".navigation_menu").slideToggle(700);
+			});
+		});
+});
+
+
+$(document).ready(function () {
+	/* Basic Gallery */
+	$('.swipebox').swipebox();
+});
