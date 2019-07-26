00fe85f6983ddcbbbf90d1736738a43bce7c68ea
dajamante <aimaiga2@gmail.com>
2019-07-26 16:03:13 +0200

    fixed swipebox

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,8 +11,19 @@ $(document).ready(function() {
 
 
 
+$(document).ready(function () {
+	/* Basic Gallery */
+	$('.swipebox').swipebox({
+		removeBarsOnMobile: false, // false will show top bar on mobile devices
+		hideBarsDelay: 30000, // delay before hiding bars on desktop
+		loopAtEnd: false // true will return to the first image after the last image is reached
+	});
+});
 
-var form = $('#contact'),
+
+
+
+var form = $('.kontakt_formular'),
     submit = form.find('[name="submit"]');
 
 form.on('submit', function(e) {
