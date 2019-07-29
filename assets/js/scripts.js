438224482d0ffd851ffccd86cf9d0c4b2c76ebcb
dajamante <aimaiga2@gmail.com>
2019-07-29 10:26:20 +0200

    moved script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,56 +9,11 @@ $(document).ready(function() {
 });
 
 
-
-
-var form = $('.kontakt_formular'),
-    submit = form.find('[name="submit"]');
-
-form.on('submit', function(e) {
-  e.preventDefault();
-
-  // avoid spamming buttons
-  if (submit.attr('value') !== 'Send')
-    return;
-
-  var valid = true;
-  form.find('input, textarea').removeClass('invalid').each(function() {
-    if (!this.value) {
-      $(this).addClass('invalid');
-      valid = false;
-    }
-  });
-
-  if (!valid) {
-    form.animate({left: '-3em'},  50)
-        .animate({left:  '3em'}, 100)
-        .animate({left:    '0'},  50);
-  } else {
-    submit.attr('value', 'Sending...')
-          .css({boxShadow: '0 0 200em 200em rgba(225, 225, 225, 0.6)',
-                backgroundColor: '#ccc'});
-    // simulate AJAX response
-    setTimeout(function() {
-      // step 1: slide labels and inputs
-      // when AJAX responds with success
-      // no animation for AJAX failure yet
-      form.find('label')
-          .animate({left: '100%'}, 500)
-          .animate({opacity: '0'}, 500);
-    }, 1000);
-    setTimeout(function() {
-      // step 2: show thank you message after step 1
-      submit.attr('value', 'Thank you :)')
-            .css({boxShadow: 'none'});
-    }, 2000);
-    setTimeout(function() {
-      // step 3: reset
-      form.find('input, textarea').val('');
-      form.find('label')
-          .css({left: '0'})
-          .animate({opacity: '1'}, 500);
-      submit.attr('value', 'Send')
-            .css({backgroundColor: ''});
-    }, 4000);
-  }
+$(document).ready(function () {
+	/* Basic Gallery */
+	$('.swipebox').swipebox({
+		removeBarsOnMobile: false, // false will show top bar on mobile devices
+		hideBarsDelay: 30000, // delay before hiding bars on desktop
+		loopAtEnd: false // true will return to the first image after the last image is reached
+	});
 });
