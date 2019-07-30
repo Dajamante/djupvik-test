00c8ea34fdb33cdc382fdde610e412f2880681f0
dajamante <aimaiga2@gmail.com>
2019-07-30 09:54:02 +0200

    change to unique names for grid areas

--- assets/js/cut.js
+++ assets/js/cut.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
 			$(".read_more_toggle button").click(function(e){
 				/* there was a conflict with swipebox */
 				e.stopPropagation();
-				$(".read_more").slideToggle(700)
+				$(".read_more").slideToggle(1000)
 
 				let target_button = $("#myBtn")[0];
 
-				if(target_button.innerText=="more..."){
-					target_button.innerText = "less!"
+				if(target_button.innerText=="more"){
+					target_button.innerText = "less"
 				}
-				else if(target_button.innerText=="less!"){
-					target_button.innerText = "more..."
+				else if(target_button.innerText=="less"){
+					target_button.innerText = "more"
 				}
 			});
 		});
