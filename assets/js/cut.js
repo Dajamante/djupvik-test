3c81422b22d77fa0f6d4e68fb152deb95b6a612f
dajamante <aimaiga2@gmail.com>
2019-07-30 08:57:21 +0200

    working readless read more

--- assets/js/cut.js
+++ assets/js/cut.js
@@ -4,6 +4,13 @@ $(document).ready(function() {
 				/* there was a conflict with swipebox */
 				e.stopPropagation();
 				$(".read_more").slideToggle(700)
+				let target_button = $("#myBtn")[0];
+				if(target_button.innerText=="Read more"){
+					target_button.innerText = "Read less"
+				}
+				else if(target_button.innerText=="Read less"){
+					target_button.innerText = "Read more"
+				}
 			});
 		});
 });
