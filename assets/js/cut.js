11d6c20cea7347363001fd6265f4c4a7a7a60484
dajamante <aimaiga2@gmail.com>
2019-07-30 09:20:26 +0200

    reverted to target_button.innerHtml

--- assets/js/cut.js
+++ assets/js/cut.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
 				/* there was a conflict with swipebox */
 				e.stopPropagation();
 				$(".read_more").slideToggle(700)
+
 				let target_button = $("#myBtn")[0];
-				if($(".read_more").is(':visible')){
+
+				if(target_button.innerText=="Read more"){
 					target_button.innerText = "Read less"
 				}
-				else{
+				else if(target_button.innerText=="Read less"){
 					target_button.innerText = "Read more"
 				}
 			});
